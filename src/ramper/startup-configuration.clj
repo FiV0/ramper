7d1d91193e7869cf2cd49c7aeb6e8216b9dacdac
FiV0 <finn.volkel@gmail.com>
2021-04-03 12:12:16 +0100

    Read config

---
+++ src/ramper/startup-configuration.clj
@@ -0,0 +1,29 @@
+(ns ramper.startup-configuration
+  "Read and check a startup-configuration."
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]))
+
+(s/def ::supported-keys (s/keys :req [:ramper/dns-threads
+                                      :ramper/fetching-threads
+                                      :ramper/parsing-threads
+                                      :ramper/max-urls
+                                      :ramper/max-urls-per-scheme-authority
+                                      :ramper/root-dir
+                                      :ramper/seed-file
+                                      :ramper/user-agent
+                                      :ramper/user-agent-from]))
+
+(defn read-config
+  "Read the config from `file` or from a default config."
+  ([] (read-config (io/file (io/resource "ramper-config.edn"))))
+  ([file]
+   (edn/read-string (slurp (io/file file)))))
+
+(s/fdef read-config
+  :ret ::supported-keys)
+
+(comment
+  (read-config)
+
+  )
