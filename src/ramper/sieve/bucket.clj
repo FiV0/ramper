3e393c8544a81ed24a4b1f1581b019ee412a095c
FiV0 <finn.volkel@gmail.com>
2021-06-30 12:29:27 +0200

    Sieve bucket (untested)

---
+++ src/ramper/sieve/bucket.clj
@@ -0,0 +1,64 @@
+(ns ramper.sieve.bucket
+  (:require [clojure.java.io :as io]
+            [ramper.util.byte-serializer :refer [from-stream to-stream skip]])
+  (:import (it.unimi.dsi.fastutil.io FastBufferedInputStream FastBufferedOutputStream)
+           (java.io FileInputStream FileOutputStream)))
+
+(defrecord Bucket [serializer items size buffer aux-file aux-in aux-out io-buffer])
+
+(defn bucket
+  "Creates a new bucket."
+  [serializer bucket-size buffer-size sieve-dir]
+  (let [aux-file (io/file sieve-dir "aux")
+        io-buffer (make-array Byte/TYPE buffer-size)]
+    (->Bucket serializer
+              0
+              bucket-size
+              (make-array Long/TYPE buffer-size)
+              aux-file
+              nil
+              (FastBufferedOutputStream. (FileOutputStream. aux-file) io-buffer)
+              io-buffer)))
+
+(defn append
+  "Add a key to the bucket."
+  [{:keys [items size buffer serializer aux-out] :as bucket} hash key]
+  {:pre [(<= items size)]}
+  (aset buffer items hash)
+  (to-stream serializer aux-out key)
+  (conj bucket {:items (inc items)}))
+
+(defn is-full?
+  "Check if the bucket is full."
+  [{:keys [items size]}]
+  (= items size))
+
+(defn prepare
+  "Prepare the bucket for consuming."
+  [{:keys [aux-out aux-file io-buffer] :as bucket}]
+  (.flush aux-out)
+  (conj bucket {:aux-in (FastBufferedInputStream. (FileInputStream. aux-file) io-buffer)}))
+
+(defn consume-key
+  "Consume a key from the bucket."
+  [{:keys [aux-in serializer]}]
+  {:pre [(-> aux-in nil? not)]}
+  (from-stream serializer aux-in))
+
+(defn skip-key
+  "Skips a key from the bucket."
+  [{:keys [aux-in serializer]}]
+  {:pre [(-> aux-in nil? not)]}
+  (skip serializer aux-in))
+
+(defn clear
+  "Clear the bucket for reuse."
+  [{:keys [aux-out aux-in] :as bucket}]
+  (.close aux-in)
+  (.position aux-out 0)
+  (conj bucket {:items 0 :aux-in nil}))
+
+(defn close
+  "Close the bucket."
+  [{:keys [aux-file]}]
+  (.delete aux-file))
