c96d66f0b1b80d519d84b4ede296cb1276709f40
FiV0 <finn.volkel@gmail.com>
2021-07-05 22:31:12 +0200

    mercator sieve imperative version (testing)

--- src/ramper/sieve/mercator_sieve.clj
+++ src/ramper/sieve/mercator_sieve.clj
@@ -10,15 +10,135 @@
   (:import (it.unimi.dsi.fastutil.ints IntArrays)
            (it.unimi.dsi.fastutil.longs LongArrays)))
 
+(defn- fill-array-consecutive
+  "Fills array `a` with consecutive number from 0 to `n`-1."
+  [^ints a n]
+  {:pre [(<= n (count a))]}
+  (loop [i 0]
+    (when (< i n)
+      (aset a i i)
+      (recur (inc i)))))
 
-
-(defrecord MercatorSieve [receiver serializer hash-function bucket store closed position]
+(deftype MercatorSieve [receiver serializer hash-function ^:volatile-mutable bucket
+                        ^:volatile-mutable store ^:volatile-mutable closed position]
   java.io.Closeable
   (close [sieve]
-    (.close bucket)
-    (-> (flush sieve) (conj {:closed true})))
+    (locking sieve
+      (bucket-api/close bucket)
+      (flush sieve)
+      (set! closed true)))
+
   Size
-  (number-of-items [this] (number-of-items bucket)))
+  (number-of-items [this] (number-of-items bucket))
+
+  Sieve
+  (enqueue [sieve key]
+    (when closed (throw (IllegalStateException.)))
+    (let [hash (hash-function key)]
+      (locking sieve
+        (set! bucket (bucket-api/append bucket hash key))
+        (when (bucket-api/is-full? bucket)
+          (flush sieve)))))
+
+  (flush [sieve]
+    (locking sieve
+      (let [start (System/nanoTime)]
+        (if (zero? (number-of-items bucket))
+          (do
+            (log/info :mercator-flush-empty {})
+            sieve)
+          (do
+            (set! store (store-api/open store))
+            (set! bucket (bucket-api/prepare bucket))
+            (let [store-size (store-api/size store)
+                  number-of-bucket-items (number-of-items bucket)
+                  _ (fill-array-consecutive position number-of-bucket-items)
+                  buffer (bucket-api/get-buffer bucket)]
+              (log/info :mercator-sorting {:number-of-items number-of-bucket-items
+                                           :position (areduce position i ret #{} (if (<= 128 (aget position i))
+                                                                                   (conj ret (aget position i))
+                                                                                   ret))})
+              (LongArrays/parallelRadixSortIndirect position buffer 0 number-of-bucket-items false)
+              (LongArrays/stabilize position buffer 0 number-of-bucket-items)
+              (loop [next (if-not (zero? store-size) (store-api/consume store) -1)
+                     store-position 0
+                     new-hashes 0
+                     j 0
+                     dups 0]
+                (if (< j number-of-bucket-items)
+                  (let [hash (aget buffer (aget position j))
+                        ;; this invalidates duplicates
+                        [new-j new-dups] (loop [k (inc j) dups dups]
+                                           (if (and (< k number-of-bucket-items)
+                                                    (= hash (aget buffer (aget position k))))
+                                             (do
+                                               (aset position k Integer/MAX_VALUE)
+                                               (recur (inc k) (inc dups)))
+                                             [k dups]))]
+                    (cond
+                      ;; no more hashes in the store
+                      ;; or the new key comes before the next key in the store
+                      (or (= store-position store-size)
+                          (< hash next))
+                      (do (store-api/append store hash)
+                          (recur next store-position (inc new-hashes) new-j new-dups))
+                      ;; existing key
+                      ;; invalidate position and get next hash from store
+                      (= hash next)
+                      (do (store-api/append store hash)
+                          (aset position j Integer/MAX_VALUE)
+                          (recur (if (< store-position (dec store-size)) (store-api/consume store) next)
+                                 (inc store-position)
+                                 new-hashes
+                                 new-j
+                                 new-dups))
+                      ;; old key, just append
+                      (< next hash)
+                      (do (store-api/append store next)
+                          (recur (if (< store-position (dec store-size)) (store-api/consume store) next)
+                                 (inc store-position)
+                                 new-hashes
+                                 new-j
+                                 new-dups))))
+                  (do
+                    ;; no more hashes in the bucket, but we still write the remaining ones
+                    ;; to the new store
+                    (loop [store-position store-position next next]
+                      (when (< store-position store-size)
+                        (store-api/append store next)
+                        (recur (inc store-position) (if (< store-position (dec store-size)) (store-api/consume store) next))))
+                    (log/info :mercator-sort+fusion-completed
+                              {:hashes (+ store-size new-hashes)
+                               :unique-key-ration (- 100.0 (* 100.0 (/ dups number-of-bucket-items)))
+                               :time (/ (- (System/nanoTime) start) 1e9)}))))
+
+              ;; adding new keys to the FlowReceiver
+              (IntArrays/parallelQuickSort position 0 number-of-bucket-items)
+              (prepare-to-append receiver)
+              (loop [j 0 bucket-position 0]
+                (if (and (< j number-of-bucket-items)
+                         (not= (aget position j) Integer/MAX_VALUE))
+                  (let [pos (aget position j)]
+                    (if (< bucket-position pos) ;; a duplicate key
+                      (do
+                        (bucket-api/skip-key bucket)
+                        (recur j (inc bucket-position)))
+                      (do
+                        (append receiver (aget buffer pos) (bucket-api/consume-key bucket))
+                        (recur (inc j) (inc bucket-position)))))
+                  ;; here j is actually equal to the number of items that made it through the sieve
+                  (let [dups (- number-of-bucket-items j)]
+                    (set! bucket (bucket-api/clear bucket))
+                    (finish-appending receiver)
+                    (log/info :mercator-end-flow-receiver-appending
+                              {:new-keys j
+                               :dups dups
+                               :throughput-ratio (- 100.0 (* 100.0 (/ dups number-of-bucket-items)))}))))
+
+              (let [duration (max (- (System/nanoTime) start) 1)]
+                (log/info :mercator-flush-completed
+                          {:total-time (/ duration 1e9)})
+                (set! store (store-api/close store))))))))))
 
 ;; TODO: think about if there should be the possibility of transforming keys before hashing
 ;; as otherwise stuff might be done multiple times for serialization and hashing
@@ -29,7 +149,7 @@
   The arguments are:
   -`new?` - is this a new or an existing seive.
   -`sieve-dir` - a directory where the sieve should be saved
-  -`sieve-size` - the size of the sieve in longs
+  -`sieve-size` - the size of the sieve in longs (before a flush is needed)
   -`store-buffer-size` - the size of the buffer in bytes used during seive flushs to read and
   write hashes (allocated twice during flushes)
   -`aux-buffer-size` - the size of the buffer to read and write an auxiliary file
@@ -48,112 +168,3 @@
                    (store-api/store new sieve-dir "store" store-buffer-size)
                    false
                    (make-array Integer/TYPE sieve-size)))
-
-(defn- fill-array-consecutive
-  "Fills array `a` with consecutive number from 0 to `n`-1."
-  [a n]
-  (loop [i 0]
-    (when (< i n)
-      (aset a i i)
-      (recur (inc i)))))
-
-(extend-type MercatorSieve
-  Sieve
-  (enqueue [{:keys [closed hash-function bucket] :as sieve} key]
-    (when closed (throw (IllegalStateException.)))
-    (let [hash (hash-function key)]
-      (locking sieve
-        (bucket-api/append bucket hash key)
-        (if (bucket-api/is-full? bucket)
-          (do (flush sieve) true)
-          false))))
-
-  (flush [{:keys [bucket store position receiver serializer] :as sieve}]
-    (locking sieve
-      (let [start (System/nanoTime)]
-        (if (zero? (number-of-items bucket))
-          (throw (IllegalStateException. "No new items in sieve."))
-          (let [store (store-api/open store)
-                store-size (store-api/size store)
-                number-of-bucket-items (number-of-items bucket)
-                _ (fill-array-consecutive position)
-                buffer (bucket-api/get-buffer bucket)]
-            (LongArrays/parallelRadixSortIndirect position buffer 0 number-of-bucket-items false)
-            (LongArrays/stabilize position buffer 0 number-of-bucket-items)
-            (loop [next (if-not (zero? store-size) (.consume store) -1)
-                   store-position 0
-                   new-hashes 0
-                   j 0
-                   dups 0]
-              (if (< j number-of-bucket-items)
-                (let [hash (aget buffer (aget position j))
-                      ;; this invalidates duplicates
-                      [new-j new-dups] (loop [j j dups dups]
-                                         (if (and (< j (dec number-of-bucket-items))
-                                                  (= hash (aget buffer (aget position j))))
-                                           (do
-                                             (aset position (inc j) Integer/MAX_VALUE)
-                                             (recur (inc j) (inc dups)))
-                                           [j dups]))]
-                  (cond
-                    ;; no more hashes in the store
-                    ;; or the new key comes before the next key in the store
-                    (or (= store-position store-size)
-                        (< hash next))
-                    (do (store-api/append store hash)
-                        (recur next store-position (inc new-hashes) new-j new-dups))
-                    ;; existing key
-                    ;; invalidate position and get next hash from store
-                    (= hash next)
-                    (do (store-api/append store hash)
-                        (aset position j Integer/MAX_VALUE)
-                        (recur (if (< store-position (dec store-size)) (store-api/consume store) next)
-                               (inc store-position)
-                               new-hashes
-                               new-j
-                               new-dups))
-                    ;; old key, just append
-                    (< next hash)
-                    (do (store-api/append store next)
-                        (recur (if (< store-position (dec store-size)) (store-api/consume store) next)
-                               (inc store-position)
-                               new-hashes
-                               new-j
-                               new-dups))))
-                (do
-                  ;; no more hashes in the bucket, but we still write the remaining ones
-                  ;; to the new store
-                  (loop [store-position store-position next next]
-                    (when (< store-position store-size)
-                      (store-api/append store next)
-                      (recur (inc store-position) (if (< store-position (dec store-size)) (store-api/consume store) next))))
-                  (log/info :mercator-sort+fusion-completed
-                            {:hashes (+ store-size new-hashes)
-                             :time (/ (- start (System/nanoTime)) 1e9)}))))
-
-            ;; adding new keys to the FlowReceiver
-            (IntArrays/parallelQuickSort position 0 number-of-bucket-items)
-            (prepare-to-append receiver)
-            (loop [j 0 bucket-position 0]
-              (if (and (< j number-of-bucket-items)
-                       (not= (aget position j) Integer/MAX_VALUE))
-                (let [pos (aget position j)]
-                  (if (< bucket-position pos) ;; a duplicate key
-                    (do
-                      (bucket-api/skip-key bucket)
-                      (recur j (inc bucket-position)))
-                    (do
-                      (append serializer (aget buffer pos) (bucket-api/consume-key bucket))
-                      (recur (inc j) (inc bucket-position)))))
-                ;; here j is actually equal to the number of items added to the bucket
-                (let [dups (- number-of-bucket-items j)]
-                  (bucket-api/clear bucket)
-                  (finish-appending receiver)
-                  (log/info :mercator-end-flow-receiver-appending
-                            {:new-keys j
-                             :dups dups
-                             :unique-key-ratio (* 100.0 (/ dups number-of-bucket-items))}))))
-            (let [duration (max (- (System/nanoTime) start) 1)]
-              (log/info :mercator-flush-completed
-                        {:total-time (/ duration 1e9)})
-              (conj sieve {:store (store-api/close store)}))))))))
---
+++ test/ramper/sieve/mercator_sieve_test.clj
@@ -0,0 +1,58 @@
+(ns ramper.sieve.mercator-sieve-test
+  (:require
+   [io.pedestal.log :as log]
+   [clojure.core.async :as async]
+   [clojure.test :refer [deftest is testing run-tests]]
+   [ramper.sieve.disk-flow-receiver :as receiver]
+   [ramper.sieve :as sieve :refer [no-more-append]]
+   [ramper.sieve.mercator-sieve :as mercator-sieve]
+   [ramper.util :as util]
+   [ramper.util.byte-serializer :as serializer]))
+
+(defn hash' [x] (-> x hash long))
+
+(deftest mercator-sieve-simple-test
+  (testing "mercator sieve simple testing"
+    (let [r (receiver/disk-flow-receiver (serializer/string-byte-serializer))
+          s (mercator-sieve/mercator-seive true (util/temp-dir "tmp-sieve") 128 32
+                                           32 r (serializer/string-byte-serializer) hash')]
+      (run! #(sieve/enqueue s %) ["A0" "A1" "A0" "A2" ])
+      (sieve/flush s)
+      (is "A0" (receiver/dequeue-key r))
+      (is "A1" (receiver/dequeue-key r))
+      (is "A2" (receiver/dequeue-key r))
+      (run! #(sieve/enqueue s %) ["A5" "A0" "A6" "A1" "A6" "A7"])
+      (sieve/flush s)
+      (.close s)
+      (no-more-append r)
+      (is "A5" (receiver/dequeue-key r))
+      (is "A6" (receiver/dequeue-key r))
+      (is "A7" (receiver/dequeue-key r)))))
+
+(defn- random-string [n]
+  (str "A" (rand-int n)))
+
+(deftest mercator-sieve-multi-threaded-sequential-dequeue
+  (testing "mercator sieve multithreaed enqueueing, sequential dequeueing"
+    (let [r (receiver/disk-flow-receiver (serializer/string-byte-serializer))
+          s (mercator-sieve/mercator-seive true (util/temp-dir "tmp-sieve") 128 32
+                                           32 r (serializer/string-byte-serializer) hash')
+          number-items (+ 100 (rand-int 2))
+          ;; (rand-int 24)
+          items  (repeatedly number-items (partial random-string number-items))
+          enqueued (atom #{})
+          nb-threads 2
+          nb-items-per-thread 25
+          threads (repeatedly nb-threads #(future
+                                            (dotimes [_ nb-items-per-thread]
+                                              (let [item (rand-nth items)]
+                                                (sieve/enqueue s item)
+                                                (swap! enqueued conj item)))))]
+      (run! deref threads)
+      (sieve/flush s)
+      (let [dequeued (reduce (fn [res _] (conj res (receiver/dequeue-key r)))
+                             #{}
+                             (range (count @enqueued)))]
+        (is (= @enqueued dequeued) "enqueued and dequeued sets must be equal")))))
+
+(mercator-sieve-multi-threaded-sequential-dequeue)
