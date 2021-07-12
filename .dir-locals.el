5d8afc3e9b5efb9d65d8f61d9393b06bb382d34e
FiV0 <finn.volkel@gmail.com>
2021-07-12 11:44:59 +0200

    Indent and linting updates for nippy

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,9 @@
          (cider-preferred-build-tool           . clojure-cli)
          (cider-redirect-server-output-to-repl . t)
          (cider-repl-display-help-banner       . nil)
-         (clojure-toplevel-inside-comment-form . t))))
+         (clojure-toplevel-inside-comment-form . t)))
+ (clojure-mode . ((eval . (define-clojure-indent
+                            (as-> 1)
+                            (match 1)
+                            (nippy/extend-freeze 2)
+                            (nippy/extend-thaw 2))))))
