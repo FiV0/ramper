4710ecb4edf41078ea63c1a238257b7de46e7267
FiV0 <finn.volkel@gmail.com>
2021-07-23 11:39:49 +0200

    Update repl-sessions.concurrent-queue

--- src/clj/repl_sessions/concurrent_queue.clj
+++ src/clj/repl_sessions/concurrent_queue.clj
@@ -50,6 +50,8 @@
 ;; => 24.954
 
 ;; persistent queue with atom and swap
+;; this test is kind of flawed as you are not actually getting the top element
+;; but only dequeueing we need to use some sort of dequeue algo, see below
 (let [start (System/currentTimeMillis)
       queue (atom clojure.lang.PersistentQueue/EMPTY)
       enqueue-threads (repeatedly  nb-threads
@@ -67,3 +69,31 @@
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
 ;; => 10.861
+
+(defn dequeue! [queue]
+  (loop []
+    (let [q     @queue
+          value (peek q)
+          nq    (pop q)]
+      (if (compare-and-set! queue q nq)
+        value
+        (recur)))))
+
+(let [start (System/currentTimeMillis)
+      queue (atom clojure.lang.PersistentQueue/EMPTY)
+      enqueue-threads (repeatedly  nb-threads
+                                   #(async/thread
+                                      (dotimes [_ items-per-thread]
+                                        (swap! queue conj (util/rand-str 12)))
+                                      true))
+      dequeue-threads (repeatedly nb-threads
+                                  #(async/thread
+                                     (loop [cnt 0]
+                                       (when (= cnt items-per-thread)
+                                         (dequeue! queue)
+                                         (recur (inc cnt))))))]
+  (run! async/<!! enqueue-threads)
+  (run! async/<!! dequeue-threads)
+  (double (/ (- (System/currentTimeMillis) start) 1000)))
+;; => 10.505
+;; wow still extremely fast
