e1a4ebcde128cbe948ca44035e002e135343a6da
FiV0 <finn.volkel@gmail.com>
2021-07-27 23:25:27 +0200

    More testing in repl-sessions.concurrent-queue

--- src/clj/repl_sessions/concurrent_queue.clj
+++ src/clj/repl_sessions/concurrent_queue.clj
@@ -97,3 +97,60 @@
   (double (/ (- (System/currentTimeMillis) start) 1000)))
 ;; => 10.505
 ;; wow still extremely fast
+
+(defn dequeue2! [queue] (ffirst (swap-vals! queue pop)))
+
+;; using simpler dequeue!
+(let [start (System/currentTimeMillis)
+      queue (atom clojure.lang.PersistentQueue/EMPTY)
+      enqueue-threads (repeatedly  nb-threads
+                                   #(async/thread
+                                      (dotimes [_ items-per-thread]
+                                        (swap! queue conj (util/rand-str 12)))
+                                      true))
+      dequeue-threads (repeatedly nb-threads
+                                  #(async/thread
+                                     (loop [cnt 0]
+                                       (when (= cnt items-per-thread)
+                                         (dequeue2! queue)
+                                         (recur (inc cnt))))))]
+  (run! async/<!! enqueue-threads)
+  (run! async/<!! dequeue-threads)
+  (double (/ (- (System/currentTimeMillis) start) 1000)))
+;; => 10.434
+
+;; using a channel
+;; this is broken
+(let [start (System/currentTimeMillis)
+      queue (async/chan 1000)
+      enqueue-threads (repeatedly  nb-threads
+                                   #(async/thread
+                                      (loop [cnt 0]
+                                        (when (< cnt items-per-thread)
+                                          ;; (when (= 0 (mod cnt 100000))
+                                          ;;   (println "foo bar"))
+                                          (let [t (async/timeout 500)
+                                                v (async/alt!! t :timeout
+                                                               [[queue (util/rand-str 12)]] :put)]
+                                            (if (= v :timeout)
+                                              (do
+                                                (Thread/sleep 100)
+                                                (recur cnt))
+                                              (recur (inc cnt))))))
+                                      true))
+      dequeue-threads (repeatedly nb-threads
+                                  #(async/thread
+                                     (loop [cnt 0]
+                                       (when (< cnt items-per-thread)
+                                         (let [t (async/timeout 500)
+                                               v (async/alt!! t :timeout
+                                                              queue :taken)]
+                                           (if (= v :timeout)
+                                             (do
+                                               (Thread/sleep 100)
+                                               (recur cnt))
+                                             (recur (inc cnt))))))
+                                     true))]
+  (run! async/<!! enqueue-threads)
+  (run! async/<!! dequeue-threads)
+  (double (/ (- (System/currentTimeMillis) start) 1000)))
