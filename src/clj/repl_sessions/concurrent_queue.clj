9ae8329289a5fb0b7ec033f78d69503e2b25a28a
FiV0 <finn.volkel@gmail.com>
2021-07-28 10:34:27 +0200

    Update repl-sessions.concurrent-queue
    Added missing doall to some tests and async/chan tests

--- src/clj/repl_sessions/concurrent_queue.clj
+++ src/clj/repl_sessions/concurrent_queue.clj
@@ -12,63 +12,63 @@
 ;; ConcurrentLinkedQueue
 (let [start (System/currentTimeMillis)
       queue (ConcurrentLinkedQueue.)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (dotimes [_ items-per-thread]
-                                        (.offer queue (util/rand-str 12)))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (cond
-                                         (= cnt items-per-thread) true
-                                         (.poll queue) (recur (inc cnt))
-                                         :else (recur cnt)))))]
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (dotimes [_ items-per-thread]
+                                               (.offer queue (util/rand-str 12)))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (cond
+                                                (= cnt items-per-thread) true
+                                                (.poll queue) (recur (inc cnt))
+                                                :else (recur cnt))))))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
-;; => 17.052
+;; => 6.766
 
 ;; queue with atomic reference
 (let [start (System/currentTimeMillis)
       queue (queue/queue)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (dotimes [_ items-per-thread]
-                                        (queue/enqueue queue (util/rand-str 12)))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (cond
-                                         (= cnt items-per-thread) true
-                                         (queue/dequeue queue) (recur (inc cnt))
-                                         :else (recur cnt)))))]
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (dotimes [_ items-per-thread]
+                                               (queue/enqueue queue (util/rand-str 12)))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (cond
+                                                (= cnt items-per-thread) true
+                                                (queue/dequeue queue) (recur (inc cnt))
+                                                :else (recur cnt))))))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
-;; => 24.954
+;; => 7.022
 
 ;; persistent queue with atom and swap
 ;; this test is kind of flawed as you are not actually getting the top element
 ;; but only dequeueing we need to use some sort of dequeue algo, see below
 (let [start (System/currentTimeMillis)
       queue (atom clojure.lang.PersistentQueue/EMPTY)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (dotimes [_ items-per-thread]
-                                        (swap! queue conj (util/rand-str 12)))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (when (= cnt items-per-thread)
-                                         (swap! queue pop)
-                                         (recur (inc cnt))))))]
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (dotimes [_ items-per-thread]
+                                               (swap! queue conj (util/rand-str 12)))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (when (= cnt items-per-thread)
+                                                (swap! queue pop)
+                                                (recur (inc cnt)))))))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
-;; => 10.861
+;; => 7.731
 
 (defn dequeue! [queue]
   (loop []
@@ -81,76 +81,97 @@
 
 (let [start (System/currentTimeMillis)
       queue (atom clojure.lang.PersistentQueue/EMPTY)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (dotimes [_ items-per-thread]
-                                        (swap! queue conj (util/rand-str 12)))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (when (= cnt items-per-thread)
-                                         (dequeue! queue)
-                                         (recur (inc cnt))))))]
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (dotimes [_ items-per-thread]
+                                               (swap! queue conj (util/rand-str 12)))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (when (= cnt items-per-thread)
+                                                (dequeue! queue)
+                                                (recur (inc cnt)))))))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
-;; => 10.505
-;; wow still extremely fast
+;; => 7.721
+;; wow still quite fast
 
 (defn dequeue2! [queue] (ffirst (swap-vals! queue pop)))
 
 ;; using simpler dequeue!
 (let [start (System/currentTimeMillis)
       queue (atom clojure.lang.PersistentQueue/EMPTY)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (dotimes [_ items-per-thread]
-                                        (swap! queue conj (util/rand-str 12)))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (when (= cnt items-per-thread)
-                                         (dequeue2! queue)
-                                         (recur (inc cnt))))))]
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (dotimes [_ items-per-thread]
+                                               (swap! queue conj (util/rand-str 12)))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (when (= cnt items-per-thread)
+                                                (dequeue2! queue)
+                                                (recur (inc cnt)))))))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
-;; => 10.434
+;; => 7.553
 
 ;; using a channel
-;; this is broken
 (let [start (System/currentTimeMillis)
-      queue (async/chan 1000)
-      enqueue-threads (repeatedly  nb-threads
-                                   #(async/thread
-                                      (loop [cnt 0]
-                                        (when (< cnt items-per-thread)
-                                          ;; (when (= 0 (mod cnt 100000))
-                                          ;;   (println "foo bar"))
-                                          (let [t (async/timeout 500)
-                                                v (async/alt!! t :timeout
-                                                               [[queue (util/rand-str 12)]] :put)]
-                                            (if (= v :timeout)
-                                              (do
-                                                (Thread/sleep 100)
-                                                (recur cnt))
-                                              (recur (inc cnt))))))
-                                      true))
-      dequeue-threads (repeatedly nb-threads
-                                  #(async/thread
-                                     (loop [cnt 0]
-                                       (when (< cnt items-per-thread)
-                                         (let [t (async/timeout 500)
-                                               v (async/alt!! t :timeout
-                                                              queue :taken)]
-                                           (if (= v :timeout)
-                                             (do
-                                               (Thread/sleep 100)
-                                               (recur cnt))
-                                             (recur (inc cnt))))))
-                                     true))]
+      queue (async/chan 5000000)
+      ;; the doall is important otherwise we get bitten by laziness
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (loop [cnt 0]
+                                               (when (< cnt items-per-thread)
+                                                 (let [t (async/timeout 500)
+                                                       v (async/alt!! t :timeout
+                                                                      [[queue (util/rand-str 12)]] :put)]
+                                                   (if (= v :timeout)
+                                                     (recur cnt)
+                                                     (recur (inc cnt))))))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (when (< cnt items-per-thread)
+                                                (let [t (async/timeout 500)
+                                                      v (async/alt!! t :timeout
+                                                                     queue :taken)]
+                                                  (if (= v :timeout)
+                                                    (recur cnt)
+                                                    (recur (inc cnt))))))
+                                            true)))]
   (run! async/<!! enqueue-threads)
   (run! async/<!! dequeue-threads)
   (double (/ (- (System/currentTimeMillis) start) 1000)))
+;; => 25.047
+;; not very good even with fairly large buffer
+
+(let [start (System/currentTimeMillis)
+      queue (async/chan 5000000)
+      enqueue-threads (doall (repeatedly  nb-threads
+                                          #(async/thread
+                                             (loop [cnt 0]
+                                               (when (< cnt items-per-thread)
+                                                 (if (async/offer! queue (util/rand-str 12))
+                                                   (recur (inc cnt))
+                                                   (recur cnt)
+                                                   )))
+                                             true)))
+      dequeue-threads (doall (repeatedly nb-threads
+                                         #(async/thread
+                                            (loop [cnt 0]
+                                              (when (< cnt items-per-thread)
+                                                (if (async/poll! queue)
+                                                  (recur (inc cnt))
+                                                  (recur cnt))))
+                                            true)))]
+  (run! async/<!! enqueue-threads)
+  (run! async/<!! dequeue-threads)
+  (double (/ (- (System/currentTimeMillis) start) 1000)))
+;; => 32.54
+;; even worse with poll!/offer!
