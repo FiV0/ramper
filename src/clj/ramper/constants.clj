80940eb2786f7dc6c4c41ac053af10ef8c9d10a6
FiV0 <finn.volkel@gmail.com>
2021-07-28 14:08:39 +0200

    ramper.constants

---
+++ src/clj/ramper/constants.clj
@@ -0,0 +1,37 @@
+(ns ramper.constants
+  (:import (java.util.concurrent TimeUnit)))
+
+;; Theses constants are copied from BUbing.
+
+(def exception-to-wait-time
+  {java.net.NoRouteToHostException                  (.. TimeUnit/HOURS (toMillis 1))
+   java.net.SocketException                         (.. TimeUnit/MINUTES (toMillis 1))
+   java.net.SocketTimeoutException                  (.. TimeUnit/MINUTES (toMillis 1))
+   java.net.UnknownHostException                    (.. TimeUnit/HOURS (toMillis 1))
+   javax.net.ssl.SSLPeerUnverifiedException         (.. TimeUnit/HOURS (toMillis 1))
+   org.apache.http.client.CircularRedirectException 0
+   org.apache.http.client.RedirectException         0
+   org.apache.http.conn.ConnectTimeoutException     (.. TimeUnit/HOURS (toMillis 1))
+   org.apache.http.ConnectionClosedException        (.. TimeUnit/MINUTES (toMillis 1))
+   org.apache.http.conn.HttpHostConnectException    (.. TimeUnit/HOURS (toMillis 1))
+   org.apache.http.NoHttpResponseException          (.. TimeUnit/MINUTES (toMillis 1))
+   org.apache.http.TruncatedChunkException          (.. TimeUnit/MINUTES (toMillis 1))
+   org.apache.http.MalformedChunkCodingException    (.. TimeUnit/MINUTES (toMillis 1))})
+
+(def exception-to-max-retries
+  {java.net.UnknownHostException 2
+   javax.net.ssl.SSLPeerUnverifiedException 0
+   org.apache.http.client.CircularRedirectException 0
+   org.apache.http.client.RedirectException 0
+   org.apache.http.conn.ConnectTimeoutException 2
+   org.apache.http.ConnectionClosedException 2
+   org.apache.http.NoHttpResponseException 2
+   org.apache.http.TruncatedChunkException 1
+   org.apache.http.MalformedChunkCodingException 1})
+
+(def exception-host-killer
+  #{java.net.NoRouteToHostException
+    java.net.UnknownHostException
+    java.net.SocketException
+    javax.net.ssl.SSLPeerUnverifiedException
+    org.apache.http.conn.ConnectTimeoutException})
