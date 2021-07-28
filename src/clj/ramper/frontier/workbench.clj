86352c13b628487d573f80e33cfa4aa0072f7171
FiV0 <finn.volkel@gmail.com>
2021-07-28 23:47:53 +0200

    Update r.frontier.workbench

--- src/clj/ramper/frontier/workbench.clj
+++ src/clj/ramper/frontier/workbench.clj
@@ -6,9 +6,12 @@
 
 ;; Workbench documentation
 ;;
-;; address-to-entry - a map from ip address to workbench-entries
+;; address-to-entry - a map from ip address to workbench-entries (in the workbench)
 ;; entries - a priority queue based on the next-fetch time of the entries
 ;; broken - number of broken workbench entries
+;;
+;; It's important that the workbench entries and in address-to-entry and entries are
+;; in sync, as otherwise the logic fails.
 
 (defrecord Workbench [address-to-entry entries broken])
 
@@ -21,7 +24,7 @@
   "Returns a workbench entry for an `ip-address`."
   [^Workbench workbench ^bytes ip-address]
   {:pre [(= 4 (.length ip-address))]}
-  (get workbench ip-address))
+  (get workbench ip-address (we/workbench-entry ip-address)))
 
 (defn nb-workbench-entries
   "Returns the number of workbench entries in the `workbench`."
@@ -43,13 +46,30 @@
     (-> entries peek we/first-visit-state)
     nil))
 
+(defn- update-workbench [^Workbench {:keys [entries address-to-entry] :as workbench} old-wb-entry new-wb-entry]
+  (let [new-entries (-> entries
+                        (dissoc old-wb-entry)
+                        (conj new-wb-entry))
+        new-address-to-entry (-> address-to-entry
+                                 (dissoc (:ip-address old-wb-entry))
+                                 (assoc (:ip-address new-wb-entry) new-wb-entry))]
+    (-> workbench
+        (assoc :entries new-entries)
+        (assoc :address-to-entry new-address-to-entry))))
+
 (defn pop-visit-state
   "Removes the next visit state from the `workbench`.
   Does not check if valid or not!"
-  [^Workbench workbench]
-  (update workbench :entries we/remove))
+  [^Workbench {:keys [entries] :as workbench}]
+  (let [old-wb-entry (peek entries)
+        new-wb-entry (we/remove old-wb-entry)]
+    (update-workbench workbench old-wb-entry new-wb-entry)))
 
 (defn add-visit-state
-  [^Workbench workbench ^VisitState visit-state]
-  ;;TODO
-  )
+  "Adds a new `visit-state` to `workbench`, creating a new workbench-entry if necessary."
+  [^Workbench workbench
+   ^VisitState {:keys [ip-address] :as visit-state}]
+  {:pre [(contains? visit-state :ip-address)]}
+  (let [old-wb-entry (get-workbench-entry workbench ip-address)
+        new-wb-entry (we/add old-wb-entry visit-state)]
+    (update-workbench workbench old-wb-entry new-wb-entry)))
