b34cdd33608a8b7265946a2cac5645c179b36ca7
FiV0 <finn.volkel@gmail.com>
2021-08-06 18:48:47 +0200

    r.workers.todo-thread-test

--- src/clj/ramper/workers/todo_thread.clj
+++ src/clj/ramper/workers/todo_thread.clj
@@ -4,14 +4,31 @@
             [ramper.runtime-configuration :as runtime-config]
             [ramper.util.thread :as thread-utils]))
 
-(defn todo-thread [{:keys [runtime-config workbench
-                           todo-queue scheme+authority-to-count] :as _thread_data}]
+(defn todo-thread
+  "This function executes the logic of moving visit-states from the
+  workbench to the todo-queue.
+
+  The given thread-data map must contain:
+
+  :runtime-config - an atom containing the runtime configuration, see
+  also ramper.runtime-configuration/runtime-config.
+
+  :workbench - an atom containing the workbench of the agent
+
+  :todo-queue - a clojure.lang.PersistentQueue to which the available
+  visit states will be added
+
+  :scheme+authority-to-count - a map mapping scheme+authority to the
+  number of path queries that have passed through the workbench"
+  [{:keys [runtime-config workbench
+           todo-queue scheme+authority-to-count] :as _thread_data}]
   (thread-utils/set-thread-name (str *ns*))
   (thread-utils/set-thread-priority Thread/MAX_PRIORITY)
   (try
-    (while (runtime-config/stop? @runtime-config)
+    (while (not (runtime-config/stop? @runtime-config))
       ;; TODO maybe make the dequeue loop explict to enable logging
-      (let [{:keys [scheme+authority] :as visit-state} (workbench/dequeue-visit-state! workbench)]
+      ;; TODO maybe enable backoff, check if loop spins
+      (when-let [{:keys [scheme+authority] :as visit-state} (workbench/dequeue-visit-state! workbench)]
         (assert (<= (get scheme+authority-to-count scheme+authority 0)
                     (:ramper/max-urls-per-scheme+authority @runtime-config)))
         (swap! todo-queue conj visit-state)))
---
+++ test/clj/ramper/workers/todo_thread_test.clj
@@ -0,0 +1,48 @@
+(ns ramper.workers.todo-thread-test
+  (:require [clojure.core.async :as async]
+            [clojure.test :refer [deftest is testing]]
+            [ramper.frontier.workbench :as workbench]
+            [ramper.frontier.workbench.visit-state :as visit-state]
+            [ramper.workers.todo-thread :as todo-thread]
+            [ramper.util :as util]
+            [ramper.util.url :as url]))
+
+(defn- create-dummy-ip [s]
+  (let [ba (byte-array 4)]
+    (doall (map-indexed #(aset-byte ba %1 %2) s))
+    ba))
+
+(comment
+  (util/ip-address->str (create-dummy-ip [1 2 3 4])))
+
+(deftest todo-thread-test
+  (testing "todo-thread"
+    (let [dummy-ip1 (create-dummy-ip [1 2 3 4])
+          dummy-ip2 (create-dummy-ip [2 3 4 5])
+          dummy-ip3 (create-dummy-ip [3 4 5 6])
+          vs1 (-> (visit-state/visit-state (url/scheme+authority "https://finnvolkel.com"))
+                  (assoc :next-fetch (util/from-now 10)
+                         :ip-address dummy-ip1))
+          vs2 (-> (visit-state/visit-state (url/scheme+authority "https://clojure.org"))
+                  (assoc :next-fetch (util/from-now 30)
+                         :ip-address dummy-ip2))
+          vs3 (-> (visit-state/visit-state (url/scheme+authority "https://news.ycombinator.com"))
+                  (assoc :next-fetch (util/from-now 20)
+                         :ip-address dummy-ip3))
+          wb (atom (reduce workbench/add-visit-state (workbench/workbench) [vs1 vs2 vs3]))
+          runtime-config (atom {:ramper/runtime-stop? false
+                                :ramper/max-urls-per-scheme+authority 10})
+          todo-queue (atom clojure.lang.PersistentQueue/EMPTY)
+          scheme+authority-to-count (atom {(url/scheme+authority "https://finnvolkel.com") 5
+                                           (url/scheme+authority "https://clojure.org") 3
+                                           (url/scheme+authority "https://news.ycombinator.com") 2})
+          thread-data {:workbench wb
+                       :runtime-config runtime-config
+                       :scheme+authority-to-count scheme+authority-to-count
+                       :todo-queue todo-queue}
+          thread (async/thread (todo-thread/todo-thread thread-data))]
+      (Thread/sleep 100)
+      (swap! runtime-config assoc :ramper/runtime-stop? true)
+      (is (true? (async/<!! thread)))
+      (is (= 3 (count @todo-queue)))
+      (is (= (list vs1 vs3 vs2) (map #(dissoc % :locked-entry) @todo-queue))))))
