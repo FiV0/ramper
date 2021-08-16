b0d0d6b4230568e8a5d5678ba9873f032c923848
FiV0 <finn.volkel@gmail.com>
2021-08-16 18:32:00 +0200

    Update r.util.lru-immutable with clojure.lang.Counted

--- src/clj/ramper/util/lru_immutable.clj
+++ src/clj/ramper/util/lru_immutable.clj
@@ -8,18 +8,26 @@
   (add [_this item]
     (cw/through-cache cache (hash-fn item) (constantly true)))
   (check [_this item]
-    (cw/lookup cache (hash-fn item))))
+    (cw/lookup cache (hash-fn item)))
+
+  clojure.lang.Counted
+  (count [_this]
+    (count @cache)))
 
 (defn create-lru-cache
   ([threshold hash-fn] (create-lru-cache {} threshold hash-fn))
   ([data threshold hash-fn]
-   (->LruCacheImmutable (cw/lru-cache-factory data :threshold threshold) hash-fn)))
+   (->LruCacheImmutable
+    (cw/lru-cache-factory (into {} (map #(vector (hash-fn %) true) data)) :threshold threshold)
+    hash-fn)))
 
 (comment
   (require '[ramper.util :as util])
+  (require '[ramper.util.lru :refer [add check]])
   (def cache (create-lru-cache {} 2 util/hash-str))
   (add cache "foo bar")
   (check cache "foo bar")
   (check cache "dafafa")
   (add cache "dafafa")
-  (add cache "dafafa1"))
+  (add cache "dafafa1")
+  (count cache))
