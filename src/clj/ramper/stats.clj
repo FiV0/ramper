ab909f3ba24b48c4e338e0bc355950ca4f90cc5c
FiVo <finn.volkel@gmail.com>
2021-08-27 16:40:17 +0200

    Fix stats-loop destructering

--- src/clj/ramper/stats.clj
+++ src/clj/ramper/stats.clj
@@ -7,13 +7,16 @@
 
 (def stats (atom {}))
 
-(defn stats-loop [stats runtime-config {:ramper/keys [urls-crawled] :as _frontier} stats-chan]
+(defn stats-loop [stats runtime-config {:keys [urls-crawled] :as _frontier} stats-chan]
   (async/go
     (try
       (while (not (runtime-config/stop? @runtime-config))
         (let [[new-stats ch] (async/alts! [stats-chan (async/timeout constants/stats-interval)])]
           (when (= ch stats-chan)
-            (swap! stats merge new-stats {:urls-crawled @urls-crawled}))))
+            (swap! stats merge new-stats {:urls-crawled @urls-crawled})))
+        #_(when-let [new-stats (async/<! stats-chan)]
+            (swap! stats merge new-stats {:urls-crawled @urls-crawled})
+            (async/<! (async/timeout constants/stats-interval))))
       (catch Throwable t
         (log/error :unexpected-ex {:ex t})))
     (log/info :graceful-shutdown {:type :stats-thread})
